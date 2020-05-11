50756ee2012180d63b686f5b07eaa5620f816024
Anneta Wamono <annetawamono@yahoo.com.au>
2020-05-11 08:08:46 -0700

    Mobile menu image changes

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,11 +1,17 @@
 var menu = document.getElementById('hamburger-menu');
 var dropdown = document.getElementsByClassName('mobile-list')[0];
+var hamburger = document.getElementById('hamburger-icon');
+var close = document.getElementById('close-icon');
 
 menu.onclick = function(e) {
   e.preventDefault();
   if (dropdown.classList.contains("close")) {
     dropdown.classList.remove("close");
+    close.classList.remove("close");
+    hamburger.classList.add("close");
   } else {
     dropdown.classList.add("close");
+    close.classList.add("close");
+    hamburger.classList.remove("close");
   }
 }
